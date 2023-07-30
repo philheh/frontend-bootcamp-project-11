572e587e2ca62ac6ade7f0ea077fce07c6860efb
philheh <zuev.phillip@gmail.com>
2023-07-30 20:00:01 +0300

    revert last fixes

--- src/getFeed.js
+++ src/getFeed.js
@@ -5,20 +5,25 @@ import formatFeed from './formatFeed.js';
 const proxiedUrl = (url) => `https://allorigins.hexlet.app/get?disableCache=true&url=${url}`;
 
 const getFeed = (url, watchedState) => axios
-  .get(proxiedUrl(url))
-  .then((res) => parse(res)) // Переместили .then от parse сюда
-  .then((parsed) => {
-    const { feed, posts } = formatFeed(parsed, url);
-
-    watchedState.feeds.unshift(feed);
-    watchedState.posts.unshift(...posts);
-    watchedState.error = null;
-    watchedState.status = 'success';
-  })
-  .catch((error) => {
-    console.log(error);
-    watchedState.error = error.name;
-    watchedState.status = 'filling';
-  });
+    .get(proxiedUrl(url))
+    .then((res) => {
+      parse(res)
+        .then((parsed) => {
+          const { feed, posts } = formatFeed(parsed, url);
+          watchedState.feeds.unshift(feed);
+          watchedState.posts.unshift(...posts);
+          watchedState.error = null;
+          watchedState.status = 'success';
+        })
+        .catch((oshibka) => {
+          watchedState.error = oshibka.message;
+          watchedState.status = 'filling';
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+      watchedState.error = error.name;
+      watchedState.status = 'filling';
+    });
 
 export default getFeed;
