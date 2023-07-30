62a370d7ea548fdff41b57422208d7202ce6f14d
philheh <zuev.phillip@gmail.com>
2023-07-30 21:18:26 +0300

    fix visited link render

--- src/renderVisitedLinks.js
+++ src/renderVisitedLinks.js
@@ -1,6 +1,6 @@
 const renderVisitedLinks = (setID) => {
   const currentVisitedID = [...setID.values()].at(-1);
-  const currentLink = document.querySelector(`[data-id="${currentVisitedID}"]`);
+  const currentLink = document.querySelector(`a[data-id="${currentVisitedID}"]`);
   currentLink.classList.remove('fw-bold');
 
   currentLink.classList.add('fw-normal');
