662163134464fd5b72eb5aecd9e10321c9d6e989
philheh <zuev.phillip@gmail.com>
2023-07-31 23:36:58 +0300

    little refactoring

--- src/application.js
+++ src/application.js
@@ -2,13 +2,8 @@ import i18next from 'i18next';
 import onChange from 'on-change';
 import validate from './validator.js';
 import resources from '../locales/index.js';
-import renderStatus from './renderStatus.js';
-import renderErrors from './renderErrors.js';
+import { render, elements } from './view.js';
 import getFeed from './getFeed.js';
-import renderFeeds from './renderFeeds.js';
-import renderPosts from './renderPosts.js';
-import renderModal from './renderModal.js';
-import renderVisitedLinks from './renderVisitedLinks.js';
 
 const app = () => {
   const i18Instance = i18next.createInstance();
@@ -27,45 +22,7 @@ const app = () => {
     modalID: null,
   };
 
-  const elements = {
-    form: document.querySelector('form'),
-    input: document.querySelector('#url-input'),
-    inputErrorP: document.querySelector('.text-danger'),
-    button: document.querySelector('[type="submit"]'),
-    feedsContainer: document.querySelector('.feeds'),
-    postsContainer: document.querySelector('.posts'),
-    modal: {
-      title: document.querySelector('.modal-title'),
-      body: document.querySelector('.modal-body'),
-      footer: document.querySelector('.modal-footer'),
-    },
-  };
-
-  const watchedState = onChange(state, (path, value, preValue) => {
-    switch (path) {
-      case 'error':
-        if (value === null) return;
-        renderErrors(elements, value, preValue, i18Instance);
-        break;
-      case 'status':
-        renderStatus(elements, value, i18Instance);
-        break;
-      case 'feeds':
-        renderFeeds(elements, state.feeds, i18Instance);
-        break;
-      case 'posts':
-        renderPosts(elements, state, i18Instance);
-        break;
-      case 'modalID':
-        renderModal(elements, state.posts, value);
-        break;
-      case 'visitedLinksIDs':
-        renderVisitedLinks(value);
-        break;
-      default:
-        throw new Error('invalid render was called');
-    }
-  });
+  const watchedState = onChange(state, render(elements, state, i18Instance));
 
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
---
+++ src/view.js
@@ -0,0 +1,48 @@
+import renderStatus from './renderStatus.js';
+import renderErrors from './renderErrors.js';
+import renderFeeds from './renderFeeds.js';
+import renderPosts from './renderPosts.js';
+import renderModal from './renderModal.js';
+import renderVisitedLinks from './renderVisitedLinks.js';
+
+const render = (elements, state, i18Instance) => (path, value, preValue) => {
+  switch (path) {
+    case 'error':
+      if (value === null) return;
+      renderErrors(elements, value, preValue, i18Instance);
+      break;
+    case 'status':
+      renderStatus(elements, value, i18Instance);
+      break;
+    case 'feeds':
+      renderFeeds(elements, state.feeds, i18Instance);
+      break;
+    case 'posts':
+      renderPosts(elements, state, i18Instance);
+      break;
+    case 'modalID':
+      renderModal(elements, state.posts, value);
+      break;
+    case 'visitedLinksIDs':
+      renderVisitedLinks(value);
+      break;
+    default:
+      throw new Error('invalid render was called');
+  }
+};
+
+const elements = {
+  form: document.querySelector('form'),
+  input: document.querySelector('#url-input'),
+  inputErrorP: document.querySelector('.text-danger'),
+  button: document.querySelector('[type="submit"]'),
+  feedsContainer: document.querySelector('.feeds'),
+  postsContainer: document.querySelector('.posts'),
+  modal: {
+    title: document.querySelector('.modal-title'),
+    body: document.querySelector('.modal-body'),
+    footer: document.querySelector('.modal-footer'),
+  },
+};
+
+export { render, elements };
